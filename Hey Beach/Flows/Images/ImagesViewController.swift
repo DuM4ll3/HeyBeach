5e88b98dc60412efe31750ebefcca979238f5d38
DuM4ll3 <tferrazrafael@gmail.com>
2018-12-27 13:16:45 +0100

    displaying images titles

--- Hey Beach/Flows/Images/ImagesViewController.swift
+++ Hey Beach/Flows/Images/ImagesViewController.swift
@@ -7,7 +7,35 @@
 //
 
 final class ImagesViewController: UIViewController, ImagesView {
+    
+    @IBOutlet var imagesTableView: UITableView!
+    
+    private var images: [Image]? {
+        didSet {
+            imagesTableView.reloadData()
+        }
+    }
+    
     func display(_ images: [Image]?) {
+        self.images = images
+    }
+}
+
+extension ImagesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath)
+        guard let image = images?[indexPath.row] else { return cell }
         
+        cell.textLabel?.text = image.name
+        return cell
     }
 }
